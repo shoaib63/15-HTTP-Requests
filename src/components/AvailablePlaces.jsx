eda8b5979313990645c930cae606b9919862d495
shoaib63 <mirakishoaib@gmail.com>
2024-08-14 12:44:31 +0330

    Handling HTTP Errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
+import { useEffect } from 'react';
+
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching , setIsFetching] = useState(true);
+  const [availablePlaces , setAvailablePlaces] = useState([]); 
+
+  useEffect(()=>{ 
+
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places'); 
+      const resData = await response.json();
+      setAvailablePlaces(resData.places); 
+      setIsFetching(false);
+    }
+
+    fetchPlaces(); 
+
+  }, []);
+  
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
